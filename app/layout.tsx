647df764e4f031bf434b8f82f99599a846f83721
Saurabhyadav0 <saurabh7678944135gzp@gmail.com>
2025-04-16 15:41:04 +0530

    updated ui in nav and css

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,25 @@ import Footer from "@/component/ui/Footer";
 import Header from "@/component/ui/Header";
 import { Providers } from "./provider";
 import "@/app/globals.css";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Campus Code",
+  description:
+    "Campus Code is a nationwide community dedicated to empowering students through impactful events, sessions, and hackathons.",
+  metadataBase: new URL("https://www.campuscode.in/"),
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className="myfonts bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300 ease-in-out">
+    <html  lang="en" suppressHydrationWarning>
+     <body className="myfonts transition-colors duration-300 ease-in-out bg-white text-black dark:bg-gray-900 dark:text-white">
         <Providers>
           <Header />
-          <main className="min-h-[calc(100vh-200px)]">
-            {children}
-          </main>
+          <main className="min-h-[calc(100vh-200px)]">{children}</main>
           <Footer />
         </Providers>
       </body>
