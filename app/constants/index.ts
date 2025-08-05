4c5a7689219a23ab5b8b12903e594eb2fd19983b
Saurabhyadav0 <saurabh7678944135gzp@gmail.com>
2025-08-05 15:07:38 +0530

    sitemap fixed

---
+++ app/constants/index.ts
@@ -0,0 +1,1 @@
+export const baseURL = "https://verify.soarx.me";
---
+++ app/sitemap.ts
@@ -0,0 +1,19 @@
+import { baseURL } from "./constants";
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+     {
+      url: `${baseURL}/`,
+    },
+    {
+      url: `${baseURL}/privacy`,
+    },
+    {
+      url: `${baseURL}/refund-policy`,
+    },
+    {
+      url: `${baseURL}/terms`,
+    },
+  ];
+}
